68a5d974b4bc710256b36091a2730ec407689776
Lachy <lachy@lachymcmillan.com>
2020-02-01 10:14:16 +1100

    add CellGridDisplay component and CSS modules
    Simple display of the CellGrid to help debugging.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classNames from 'classnames'
+import { CellGrid, Cell, countAliveNeighbours } from './cellSimulatorLib';
+import styles from './App.module.css';
+
+const toCell = (cell: string) => (cell !== ' ' ? 'alive' : 'dead')
+
+const testFrame1: Readonly<CellGrid> = [ 
+//  0   1   2   3   4   5
+  [' ',' ',' ',' ',' ',' '], // 0
+  [' ',' ','X',' ',' ',' '], // 1
+  [' ',' ',' ','X',' ',' '], // 2
+  [' ','X','X','X',' ',' '], // 3
+  [' ',' ',' ',' ',' ',' '], // 4
+  [' ',' ',' ',' ',' ',' ']  // 5
+].map(row => row.map(toCell)) 
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <CellGridDisplay
+        grid={testFrame1} 
+      />
+
     </div>
   );
 }
+
+interface CellGridDisplayProps {
+  grid: Readonly<CellGrid>,
+}
+
+const CellGridDisplay: React.FC<CellGridDisplayProps> = ({ grid }: CellGridDisplayProps) => {
+  return (
+    <div className={styles.container}>
+      {grid.map((row, y) =>
+        <div className={styles.row}>
+          {row.map((cell, x) => 
+            <CellDisplay 
+              cell={cell} 
+              neighboursCount={countAliveNeighbours(x, y, grid)} 
+            />
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+interface CellDisplayProps {
+  cell: Cell,
+  neighboursCount?: number
+}
+
+const CellDisplay: React.FC<CellDisplayProps> = ({ cell, neighboursCount }: CellDisplayProps) => {
+  const classes = classNames(styles.CellDisplay, styles[cell])
+  return (
+    <div className={classes}>{neighboursCount}</div>
+  )
+}
+
 
 export default App;
