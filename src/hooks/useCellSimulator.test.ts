b5e76a7ba910aa4b46ba4bef4c522a81a07f278c
Lachy <lachy@lachymcmillan.com>
2020-02-03 14:10:58 +1100

    make useCellSimulator functional & add tests

---
+++ src/hooks/useCellSimulator.test.ts
@@ -0,0 +1,96 @@
+import { 
+  _gotoNextGen,
+  _gotoPrevGen,
+  _clear,
+  _reset,
+  _toggleCell
+} from "./useCellSimulator"
+
+import NonEmptyStack from '../lib/NonEmptyStack'
+import { CellGrid, CellState, createEmptyGrid } from '../lib/cellSimulatorLib'
+
+/** helper function to convert string cell representation into Cell type */
+const toCell = (str: string): CellState => (str !== ' ' ? 'alive' : 'dead')
+
+// the first frame of test image
+// https://user-images.githubusercontent.com/7149052/53603476-bfb00e00-3c05-11e9-8862-1dfd31836dcd.jpg
+const testFrame1: CellGrid = [ 
+//  0   1   2   3   4   5
+  [' ',' ',' ',' ',' ',' '], // 0
+  [' ',' ','X',' ',' ',' '], // 1
+  [' ',' ',' ','X',' ',' '], // 2
+  [' ','X','X','X',' ',' '], // 3
+  [' ',' ',' ',' ',' ',' '], // 4
+  [' ',' ',' ',' ',' ',' ']  // 5
+].map(row => row.map(toCell)) 
+
+const testFrame2: CellGrid = [ 
+//  0   1   2   3   4   5
+  [' ',' ',' ',' ',' ',' '], // 0
+  [' ',' ',' ',' ',' ',' '], // 1
+  [' ','X',' ','X',' ',' '], // 2
+  [' ',' ','X','X',' ',' '], // 3
+  [' ',' ','X',' ',' ',' '], // 4
+  [' ',' ',' ',' ',' ',' ']  // 5
+].map(row => row.map(toCell)) 
+
+test("_gotoNextGen adds the next generation to the top of the stack", () => {
+  const stack = new NonEmptyStack<CellGrid>([testFrame1])
+  const nextState = _gotoNextGen(stack)
+  expect(nextState.top()).toStrictEqual(testFrame2)
+})
+
+test("_gotoNextGen does not change the state if the next-gen is no different from the last gen", () => {
+  const emptyBoard = createEmptyGrid(6,6)
+  const stack = new NonEmptyStack<CellGrid>([emptyBoard])
+  const nextState = _gotoNextGen(stack)
+  expect(nextState.top()).toStrictEqual(emptyBoard)
+})
+
+test("_gotoPrevGen ", () => {
+  const stack = new NonEmptyStack<CellGrid>([testFrame1, testFrame2])
+  const nextState = _gotoPrevGen(stack)
+  expect(nextState.top()).toStrictEqual(testFrame1)
+})
+
+test("_gotoPrevGen does not change the state if prev-gen is the original configuration of the board", () => {
+  const stack = new NonEmptyStack<CellGrid>([testFrame1, testFrame2])
+  const nextState = _gotoPrevGen(stack)
+  expect(nextState.top()).toStrictEqual(testFrame1)
+  const thirdState = _gotoPrevGen(nextState)
+  expect(thirdState.top()).toStrictEqual(testFrame1)
+})
+
+test("_reset returns the game board to the user's configuration", () => {
+  let stack = new NonEmptyStack<CellGrid>([testFrame1, testFrame2])
+  stack = _gotoNextGen(stack)
+  stack = _gotoNextGen(stack)
+  stack = _gotoNextGen(stack)
+  stack = _gotoNextGen(stack)
+  expect(stack.top()).not.toStrictEqual(testFrame1)
+  stack = _reset(stack)
+  expect(stack.top()).toStrictEqual(testFrame1)
+})
+
+test("_clear makes a new board (no history) with all cells dead", () => {
+  let stack = new NonEmptyStack<CellGrid>([testFrame1, testFrame2])
+  stack = _clear(stack)
+  expect(stack.top()).toStrictEqual(createEmptyGrid(6,6))
+})
+
+test("_toggleCell changes a cell between alive/dead and clears history", () => {
+  let stack = new NonEmptyStack<CellGrid>([testFrame1, testFrame2])
+  expect(stack.top()[0][0]).toBe('dead')
+  expect(stack.length()).toBe(2)
+  // dead cell comes alive
+  stack = _toggleCell(stack, 0,0)
+  expect(stack.top()[0][0]).toBe('alive')
+  // history reset
+  expect(stack.length()).toBe(1)
+  // alive cell becomes dead
+  stack = _toggleCell(stack, 0,0)
+  expect(stack.top()[0][0]).toBe('dead')
+  // confirm nothing else has changed
+  expect(stack.top()).toStrictEqual(testFrame2)
+})
+
--- src/hooks/useCellSimulator.ts
+++ src/hooks/useCellSimulator.ts
@@ -2,57 +2,81 @@ import { useState } from 'react'
 import { CellGrid, generateNextGen, createEmptyGrid } from '../lib/cellSimulatorLib';
 import NonEmptyStack from '../lib/NonEmptyStack'
 
+/** 
+ * Provides the game board and functions for playing the cell-simulator game
+ */
 const useCellSimulator = (
   initialState: CellGrid = createEmptyGrid(6,6)
 ) => {
-  const [gridStack, set] = useState(new NonEmptyStack<CellGrid>([initialState]))
-
-  /** Generate next generation and place on top of the history stack */
-  const gotoNextGen = () => {
-    const nextGen = generateNextGen(gridStack.top())
-    // this is a quick version of a deep compare
-    if (JSON.stringify(nextGen) !== JSON.stringify(gridStack.top())) {
-      // only add history if the board has changed
-      set(gridStack.push(nextGen))
-    }
-  }
-
-  /** Return to the previous generation of the CellGrid */ 
-  const gotoPrevGen = () => 
-    // minimum of one grid on the stack
-    (gridStack.length() > 1 && set(gridStack.pop()))
-
-  /** Return to the original user configuration before next generation */
-  const reset = () => 
-    set(new NonEmptyStack<CellGrid>([gridStack.bottom()]))
-
-  /** Set all cells in CellGrid to dead */
-  const clear = () => {
-    const initial = gridStack.bottom()
-    const rows = initial.length
-    const columns = initial[0].length
-    const grid = createEmptyGrid(rows, columns)
-    // create a new bottom of the stack, so if user presses it will take them 
-    // back to the empty board at this stage 
-    set(new NonEmptyStack<CellGrid>([grid]))
-  }
-
-  /** Switch specified cell between alive/dead */
-  const toggleCell = (xPos: number, yPos: number) => {
-    // toggling also resets the history
-    const grid = gridStack.top()
-    grid[yPos][xPos] = (grid[yPos][xPos] === 'alive' ? 'dead' : 'alive')
-    set(new NonEmptyStack<CellGrid>([grid]))
-  }
+  const [gridStack, setStack] = useState(new NonEmptyStack<CellGrid>([initialState]))
 
   return { 
     cellGrid: gridStack.top(), 
-    gotoNextGen, 
-    gotoPrevGen, 
-    reset, 
-    clear, 
-    toggleCell 
+
+    gotoNextGen: () => setStack(_gotoNextGen), 
+
+    /** Return to the previous generation of the CellGrid */ 
+    gotoPrevGen: () => setStack(_gotoPrevGen), 
+
+    /** Return to the original user configuration before next generation */
+    reset: () => setStack(_reset), 
+
+    /** Set all cells in CellGrid to dead */
+    clear: () => setStack(_clear), 
+
+    /** Switch specified cell between alive/dead */
+    toggleCell: (xPos: number, yPos: number) => 
+      setStack(prevStack => _toggleCell(prevStack, xPos, yPos)) 
   }
 }
 
 export default useCellSimulator;
+
+/** Private functions - exported for testing only */
+
+export type CellGridStack = NonEmptyStack<CellGrid> // reduce typing
+
+export const _gotoNextGen = (gridStack: CellGridStack): CellGridStack => {
+  const nextGen = generateNextGen(gridStack.top())
+  // this is a quick version of a deep compare
+  const hasBoardChanged = (
+    JSON.stringify(nextGen) !== JSON.stringify(gridStack.top())
+  )
+  // only add history if the board has changed
+  return (hasBoardChanged
+    ? gridStack.push(nextGen)
+    : gridStack
+  )
+}
+
+export const _gotoPrevGen = (gridStack: CellGridStack): CellGridStack => {
+  // minimum of one grid on the stack
+  return (gridStack.length() > 1
+    ? gridStack.pop()
+    : gridStack
+  )
+}
+
+export const _reset = (gridStack: CellGridStack): CellGridStack => 
+  new NonEmptyStack<CellGrid>([gridStack.bottom()])
+
+export const _clear = (gridStack: CellGridStack): CellGridStack => {
+  const initial = gridStack.bottom()
+  const rows = initial.length
+  const columns = initial[0].length
+  const grid = createEmptyGrid(rows, columns)
+  // create a new bottom of the stack, so if user presses it will take them 
+  // back to the empty board at this stage 
+  return new NonEmptyStack<CellGrid>([grid])
+}
+
+export const _toggleCell = (
+  gridStack: CellGridStack, 
+  xPos: number, 
+  yPos: number
+): CellGridStack => {
+  // toggling also resets the history
+  const grid = gridStack.top()
+  grid[yPos][xPos] = (grid[yPos][xPos] === 'alive' ? 'dead' : 'alive')
+  return new NonEmptyStack<CellGrid>([grid])
+}
